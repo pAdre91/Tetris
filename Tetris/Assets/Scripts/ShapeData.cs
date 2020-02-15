4873d5c1af70ecf749ffd5c90a9041a43fae1693
petr.osipov91@mail.ru <petr.osipov91@mail.ru>
2020-02-15 16:56:03 +0600

    Скриптовые объекты для тетрамино

---
+++ Tetris/Assets/Scripts/ShapeData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Shapes/Standart Shape", fileName = "New Shape")]
+class ShapeData : ScriptableObject 
+{
+	[Tooltip("Основной спарйт")]
+	[SerializeField] private Sprite m_mainSprite;
+	public Sprite MainSprite
+	{
+		get { return m_mainSprite; }
+	}
+}
